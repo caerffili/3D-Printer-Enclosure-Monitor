f99b1dcda71361a8c5e88f326c620415f4d3b54b
Michael <michael.morgan@caerffili.co.yk>
2019-12-29 20:58:10 +0000

    1

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,21 @@ PubSubClient mqttClient(wifiClient);
 
 long _millis;
 
+//#define DebugOctoPrintApi
+
+#define toolQty 1
+#define enclosureTempSensors 3
+#define enclosureTempSensorBottom 0
+#define enclosureTempSensorTop 1
+#define enclosureTempSensorAmbient 2
+
+// Variables to hold current state
+float bedTemperature;
+float toolTemperature[toolQty];
+float enclosureTemperature[enclosureTempSensors];
+float jobCompletion;
+char jobState[20];
+
 void rootPage()
 {
   char content[] = "Hello, world";
@@ -103,27 +118,37 @@ bool OctoPrintGetVersion(char *oServer, char *api, char *server, char *descripti
   strcat(url, oServer);
   strcat(url, "/api/version");
 
+#ifdef DebugOctoPrintApi
   Serial.print("Making rest call to ");
   Serial.println(url);
+#endif
 
   HTTPClient http; //Object of class HTTPClient
   http.setTimeout(1000);
   http.begin(url);
   int httpCode = http.GET();
+
+#ifdef DebugOctoPrintApi
   Serial.print("Return Code: ");
   Serial.print(httpCode);
+#endif
 
   if (httpCode > 0)
   {
+#ifdef DebugOctoPrintApi
     Serial.println(" Succeeded.");
+#endif
+
     const size_t bufferSize = JSON_OBJECT_SIZE(3) + 60;
 
     DynamicJsonDocument jsonBuffer(bufferSize);
     DeserializationError error = deserializeJson(jsonBuffer, http.getString());
     if (error)
     {
+#ifdef DebugOctoPrintApi
       Serial.println("Unable to deserialize Json");
       Serial.println(error.c_str());
+#endif
     }
 
     strcpy(api, jsonBuffer["api"].as<char *>());
@@ -133,7 +158,124 @@ bool OctoPrintGetVersion(char *oServer, char *api, char *server, char *descripti
   }
   else
   {
+#ifdef DebugOctoPrintApi
     Serial.println(" No response.");
+#endif
+  }
+
+  http.end(); //Close connection
+
+  return retval;
+}
+
+bool OctoPrintGetPrinter(char *oServer, float *bedtemperature, float *tool0temperature, char *state)
+{
+  bool retval = false;
+  char url[100];
+  strcpy(url, "http://");
+  strcat(url, oServer);
+  strcat(url, "/api/printer");
+
+#ifdef DebugOctoPrintApi
+  Serial.print("Making rest call to ");
+  Serial.println(url);
+#endif
+
+  HTTPClient http; //Object of class HTTPClient
+  http.setTimeout(1000);
+  http.begin(url);
+  int httpCode = http.GET();
+
+#ifdef DebugOctoPrintApi
+  Serial.print("Return Code: ");
+  Serial.print(httpCode);
+#endif
+
+  if (httpCode > 0)
+  {
+#ifdef DebugOctoPrintApi
+    Serial.println(" Succeeded.");
+#endif
+
+    const size_t bufferSize = JSON_OBJECT_SIZE(1) + 2 * JSON_OBJECT_SIZE(2) + 3 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(11) + 199;
+
+    DynamicJsonDocument jsonBuffer(bufferSize);
+    DeserializationError error = deserializeJson(jsonBuffer, http.getString());
+    if (error)
+    {
+#ifdef DebugOctoPrintApi
+      Serial.println("Unable to deserialize Json");
+      Serial.println(error.c_str());
+#endif
+    }
+
+    *bedtemperature = jsonBuffer["temperature"]["bed"]["actual"];
+    *tool0temperature = jsonBuffer["temperature"]["tool0"]["actual"];
+    strcpy(state, jsonBuffer["state"]["text"].as<char *>());
+    retval = true;
+  }
+  else
+  {
+#ifdef DebugOctoPrintApi
+    Serial.println(" No response.");
+#endif
+  }
+
+  http.end(); //Close connection
+
+  return retval;
+}
+
+bool OctoPrintGetJobPrinter(char *oServer, float *completion, char *state)
+{
+  bool retval = false;
+  char url[100];
+  strcpy(url, "http://");
+  strcat(url, oServer);
+  strcat(url, "/api/job");
+
+#ifdef DebugOctoPrintApi
+  Serial.print("Making rest call to ");
+  Serial.println(url);
+#endif
+
+  HTTPClient http; //Object of class HTTPClient
+  http.setTimeout(1000);
+  http.begin(url);
+  int httpCode = http.GET();
+
+#ifdef DebugOctoPrintApi
+  Serial.print("Return Code: ");
+  Serial.print(httpCode);
+#endif
+
+  if (httpCode > 0)
+  {
+#ifdef DebugOctoPrintApi
+    Serial.println(" Succeeded.");
+#endif
+
+    const size_t bufferSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + 2 * JSON_OBJECT_SIZE(6) + 409;
+
+    DynamicJsonDocument jsonBuffer(bufferSize);
+    DeserializationError error = deserializeJson(jsonBuffer, http.getString());
+    if (error)
+    {
+#ifdef DebugOctoPrintApi
+      Serial.println("Unable to deserialize Json");
+      Serial.println(error.c_str());
+#endif
+    }
+
+    *completion = jsonBuffer["progress"]["completion"];
+    strcpy(state, jsonBuffer["state"].as<char *>());
+    retval = true;
+  }
+  else
+  {
+#ifdef DebugOctoPrintApi
+    Serial.println(" No response.");
+#endif
   }
 
   http.end(); //Close connection
@@ -152,27 +294,36 @@ bool OctoPrintEnclosureGetTemperature(char *oServer, int id, float *temperature)
   strcat(url, "/plugin/enclosure/inputs/");
   strcat(url, sid);
 
+#ifdef DebugOctoPrintApi
   Serial.print("Making rest call to ");
   Serial.println(url);
+#endif
 
   HTTPClient http; //Object of class HTTPClient
   http.setTimeout(1000);
   http.begin(url);
   int httpCode = http.GET();
+
+#ifdef DebugOctoPrintApi
   Serial.print("Return Code: ");
   Serial.print(httpCode);
+#endif
 
   if (httpCode > 0)
   {
+#ifdef DebugOctoPrintApi
     Serial.println(" Succeeded.");
+#endif
     const size_t bufferSize = JSON_OBJECT_SIZE(19) + 396;
 
     DynamicJsonDocument jsonBuffer(bufferSize);
     DeserializationError error = deserializeJson(jsonBuffer, http.getString());
     if (error)
     {
+#ifdef DebugOctoPrintApi
       Serial.println("Unable to deserialize Json");
       Serial.println(error.c_str());
+#endif
     }
 
     *temperature = jsonBuffer["temp_sensor_temp"];
@@ -180,7 +331,9 @@ bool OctoPrintEnclosureGetTemperature(char *oServer, int id, float *temperature)
   }
   else
   {
+#ifdef DebugOctoPrintApi
     Serial.println(" No response.");
+#endif
   }
 
   http.end(); //Close connection
@@ -239,8 +392,17 @@ void pollTemperatures()
   for (int i = 0; i < dallasDeviceCount; i++)
   {
     float temperatureC = dallasSensors.getTempCByIndex(i);
-    Serial.print(temperatureC);
-    Serial.println("ºC");
+    enclosureTemperature[i] = temperatureC;
+  }
+
+  float bedtemperature;
+  float tool0temperature;
+  char state[20];
+
+  if (OctoPrintGetPrinter("10.20.0.147", &bedtemperature, &tool0temperature, state))
+  {
+    bedTemperature = bedtemperature;
+    toolTemperature[0] = tool0temperature;
   }
 
   Serial.println("Polling OctoPrint Enclosure Temperature Sensors.");
@@ -249,11 +411,20 @@ void pollTemperatures()
   {
     if (OctoPrintEnclosureGetTemperature("10.20.0.147", i, &temperature))
     {
-      Serial.print("Temperature Sensor ");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(temperature);
+      enclosureTemperature[i - 1] = temperature;
     }
+  }
+}
+
+void pollState()
+{
+  float completion;
+  char state[20];
+
+  if (OctoPrintGetJobPrinter("10.20.0.147", &completion, state))
+  {
+    jobCompletion = completion;
+    strcpy(jobState, state);
   }
 }
 
@@ -283,19 +454,16 @@ void setup()
   digitalWrite(BUILTIN_LED1, HIGH); // Turn the LED off by making the voltage HIGH
   digitalWrite(BUILTIN_LED2, LOW);  // Turn the LED ON by making the voltage LOW
 
-  // Start up the library
+  // Start up the Dallas Sensor library
   DeviceAddress Thermometer;
   dallasSensors.begin();
 
   // locate devices on the bus
-  Serial.println("Locating devices...");
+  Serial.println("Locating Dallas devices...");
   Serial.print("Found ");
   dallasDeviceCount = dallasSensors.getDeviceCount();
   Serial.print(dallasDeviceCount, DEC);
-  Serial.println(" devices.");
-  Serial.println("");
-
-  Serial.println("Printing addresses...");
+  Serial.println(" devices. Addresses...");
   for (int i = 0; i < dallasDeviceCount; i++)
   {
     Serial.print("Sensor ");
@@ -305,15 +473,18 @@ void setup()
     printAddress(Thermometer);
   }
 
+  Serial.println("");
   Server.on("/", rootPage);
   if (Portal.begin())
   {
-    Serial.println("HTTP server:" + WiFi.localIP().toString());
+    Serial.println("HTTP server listening on:" + WiFi.localIP().toString());
   }
 
   digitalWrite(BUILTIN_LED1, LOW); // Turn the LED off by making the voltage HIGH
   digitalWrite(BUILTIN_LED2, LOW); // Turn the LED ON by making the voltage LOW
   delay(1000);
+
+  Serial.println("");
   mqttClient.setServer(MQTTSserver, 1883);
   mqttClient.setCallback(subscribeReceive);
   MQTTCconnect();
@@ -322,6 +493,8 @@ void setup()
   char server[20];
   char description[20];
 
+  Serial.println("");
+  Serial.println("Finding OctoPrint systen...");
   if (OctoPrintGetVersion("10.20.0.147", api, server, description))
   // ConnectOctoPrint();
   {
@@ -337,8 +510,6 @@ void setup()
     Serial.println("OctoPrint not connected!!");
   }
 
-
-
   _millis = millis();
 }
 
@@ -348,15 +519,56 @@ void loop()
 
   mqttClient.loop();
 
-  if ( millis() > _millis+ 2000)
+  if (millis() > _millis + 5000)
   {
-    _millis = millis();
 
     // MakeRestCall("http://jsonplaceholder.typijjhcode.com/users/1");
-      pollTemperatures();
+    Serial.println("");
+    pollTemperatures();
+    pollState();
+
+    Serial.print("Bed Temperature: ");
+    Serial.print(bedTemperature);
+    Serial.println("c");
+
+    for (int i = 0; i < toolQty; i++)
+    {
+      Serial.print("Tool Temperature ");
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(toolTemperature[i]);
+      Serial.println("c");
+    }
+
+    if (enclosureTempSensorAmbient >= 0)
+    {
+      Serial.print("Ambient Temperature: ");
+      Serial.print(enclosureTemperature[enclosureTempSensorAmbient]);
+      Serial.println("c");
+    }
+
+    if (enclosureTempSensorTop >= 0)
+    {
+      Serial.print("Enclosure Temperature (Top): ");
+      Serial.print(enclosureTemperature[enclosureTempSensorTop]);
+      Serial.println("c");
+    }
+    if (enclosureTempSensorBottom >= 0)
+    {
+      Serial.print("Enclosure Temperature (Bottom): ");
+      Serial.print(enclosureTemperature[enclosureTempSensorBottom]);
+      Serial.println("c");
+    }
+
+    Serial.print("Progress: ");
+    Serial.print(jobCompletion);
+    Serial.println("%");
+
+    Serial.print("State: ");
+    Serial.println(jobState);
+
+    _millis = millis();
   }
-
-
 }
 
 void subscribeReceive(char *topic, byte *payload, unsigned int length)
